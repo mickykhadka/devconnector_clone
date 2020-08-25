da17c8e3eca2927d4bb76c5b0118ba3cc56587b2
manojlkkhadka <manojlkkhadka@gmail.com>
2020-08-25 11:10:31 +0545

    redux starter file

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
 // import axios from "axios";
 import { Link } from "react-router-dom";
-const Register = () => {
+
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,7 +19,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("password do not match");
+      setAlert("password do not match", "danger");
     } else {
       //   const newUser = {
       //     name,
@@ -99,4 +102,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default connect(null, { setAlert })(Register);
